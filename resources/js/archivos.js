9fdeaf8f6e74dfc1c2df7646f6f17001040cd322
Daniela Agudelo <daniiagudelom@gmail.com>
2023-11-23 17:57:21 -0500

    Administrador de archivos

---
+++ resources/js/archivos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    //1. llamar a la api y obtener todos los archivos del usuario especifico
+
+    //2. mostrar los archivos en el html recorriendolos y añadiendolos a la grid
+
+})
